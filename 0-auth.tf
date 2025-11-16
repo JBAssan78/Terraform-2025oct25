3ff6e058bb64d7b2be100545e6bc2c6365d63651
JAssan <jassan888@yahoo.com>
2025-11-16 14:35:35 -0500

    tracking work

---
+++ 0-auth.tf
@@ -0,0 +1,25 @@
+# Terraform configuration block
+# This defines the minimum Terraform version and required providers
+terraform {
+  required_version = ">= 1.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+  }
+}
+
+# AWS Provider configuration
+# This tells Terraform how to connect to AWS
+provider "aws" {
+  region  = "us-east-2"
+
+  default_tags{
+    tags ={
+        Environment = "Test"
+        Managedby = "Mecca"
+    }
+  }
+}
