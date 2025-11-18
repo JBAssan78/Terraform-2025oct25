a277819595bd1ef6ccdb6680a243dc8f0942c91e
JAssan <jassan888@yahoo.com>
2025-11-18 04:58:54 -0500

    ingress added

--- 6-sg.tf
+++ 6-sg.tf
@@ -3,6 +3,13 @@ resource "aws_security_group" "app1" {
   description =  "Allow HTTP and SSH for web server"
   vpc_id = aws.vpc.app1.id
 
+  ingress = {
+    from_port = 80
+    to_port = 80
+    protocol = "TCP"
+    cidr_block = ["0.0.0.0/0"]
+  }
+
 tags ={
     Name = "demo-vpc"
 }
